3e4eaa009e44a48c5324a37e27cee8acde464c99
LOCO185 <jloco151@gmail.com>
2022-08-20 19:19:34 +0300

    commit

--- rawan-eyebrows/src/components/Calendar.js
+++ rawan-eyebrows/src/components/Calendar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { Component } from "react";
+import BigCalendar from "react-big-calendar";
+import moment from "moment";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import TextField from "material-ui/TextField";
+import TimePicker from "material-ui/TimePicker";
+require("react-big-calendar/lib/css/react-big-calendar.css");
 
 const Calendar = () => {
-  return (
-    <div>Calendar</div>
-  )
-}
+  return <div>Calendar</div>;
+};
 
-export default Calendar
+export default Calendar;
