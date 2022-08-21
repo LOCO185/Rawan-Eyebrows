7395ca5284174a9e36025764bcb80b7134c879cd
LOCO185 <jloco151@gmail.com>
2022-08-21 14:00:09 +0300

    ..

--- rawan-eyebrows/src/components/Calendar.js
+++ rawan-eyebrows/src/components/Calendar.js
@@ -10,24 +10,6 @@ require("react-big-calendar/lib/css/react-big-calendar.css");
 BigCalendar.momentLocalizer(moment);
 
 const Calendar = () => {
-  constructor() {
-    super();
-    this.state = {
-      events: [],
-      title: "",
-      start: "",
-      end: "",
-      desc: "",
-      openSlot: false,
-      openEvent: false,
-      clickedEvent: {}
-    };
-    this.handleClose = this.handleClose.bind(this);
-  }
-    //closes modals
-    // handleClose() {
-    //   this.setState({ openEvent: false, openSlot: false });
-    // }
   return <div>Calendar</div>;
 };
 
